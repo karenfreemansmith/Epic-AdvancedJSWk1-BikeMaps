a87d2ed0fac2c323660e63de51da2709df0a71bf
Alexander Jacks and Karen Freeman-Smith <alexander+karen@>
2016-10-13 15:56:58 -0700

    work except for multiple map points

--- src/js/bike.js
+++ src/js/bike.js
@@ -4,33 +4,35 @@ var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var labelIndex = 0;
 
 function initialize() {
-  var portland = { lat:45.879, lng: -122.987 };
+  var portland = { lat:40.879, lng: -100.987 };
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 9,
     center: portland
   });
-
-  // Add a marker at the center of the map.
-  addMarker(portland, map);
 
   // This event listener calls addMarker() when the map is clicked.
   google.maps.event.addListener(map, 'click', function(event) {
     console.log(event.latLng);
-    addMarker(event.latLng, map);
+
     // Adding bikes object
-    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity='+event.latLng.lat() +'%2C'+ event.latLng.lng() +'&proximity_square=200', function(response){
+    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity='+event.latLng.lat() +'%2C'+ event.latLng.lng() +'&proximity_square=200').then(function(response){
       var address;
       var location;
+      var image;
+      var description;
+      console.log(response);
       var geocoder = new google.maps.Geocoder();
       for(var i=0; i<25; i++) {
         address=response.bikes[i].stolen_location;
+        image=response.bikes[i].large_img;
+        description=response.bikes[i].frame_model;
         geocoder.geocode({'address': address}, function(results, status) {
           if (status === 'OK') {
             location = {
               lat:results[0].geometry.location.lat(),
               lng:results[0].geometry.location.lng()
             };
-            addMarker(location, map);
+            addMarker(location, map, address, image, description);
           } else {
             alert('Geocode was not successful for the following reason: ' + status);
           }
@@ -41,13 +43,32 @@ function initialize() {
 }
 
 // Adds a marker to the map.
-function addMarker(location, map) {
-  // Add the marker at the clicked location, and add the next-available label
-  // from the array of alphabetical characters.
+function addMarker(location, map, address, image, description) {
+  //Add the marker at the clicked location, and add the next-available label
+  //from the array of alphabetical characters.
+  var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h2>'+ description +'</h2>'+
+    '<div class="bodyContent">'+
+    '<p><img src="'+ image +'"></p>'+
+    '<p>'+ address +'</p>'
+    '</div>'+
+    '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
   var marker = new google.maps.Marker({
     position: location,
     label: labels[labelIndex++ % labels.length],
-    map: map
+    map: map,
+    title: address
+  });
+  
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
   });
 }
 
